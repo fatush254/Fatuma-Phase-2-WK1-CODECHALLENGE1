a63d476110c4f33c763b0736b6d7f8f1adc719ca
fatush254 <fgalgalo945@gmail.com>
2024-04-29 20:00:48 +0300

    complete

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,31 +2,31 @@ import React from 'react'
 import "./Table.css"
 
 function Table() {
-    return (
-        <div>
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Category</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                    <td>date</td>
-                    <td>description</td>
-                    <td>category</td>
-                    <td>amount</td>
-                </tr>
-            </tbody>
-        </table>
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>date</td>
+            <td>description</td>
+            <td>category</td>
+            <td>amount</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    
-    )
+
+  )
 }
 
 export default Table
